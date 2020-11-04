7bd67d80b4f9f0bbc1cf53007cc2085b6e6d9387
ThunderSword99 <guardian.of.gods99@gmail.com>
2020-11-04 11:57:22 +0700

    make hosting gui

---
+++ Assets/Scripts/Networking/HostGame.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour
+{
+    [SerializeField]
+    private uint roomSize = 4;
+
+    private string roomName;
+
+    private NetworkManager networkManager;
+
+    void Start ()
+    {
+        networkManager = NetworkManager.singleton;
+        if(networkManager.matchMaker==null)
+        {
+            networkManager.StartMatchMaker();
+        }
+    }
+
+    public void SetRoom(string _name)
+    {
+        roomName = _name;
+    }
+
+    public void CreateRoom()
+    {
+        if (roomName != "" && roomName!=null)
+        {
+            Debug.Log("Creating room " +  roomName + ", room size: " + roomSize);
+
+            networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+        }
+    }
+}
