6b47b6dfb1e492e3fa666732335fe3b294c6fc9a
kocochj999 <kocochj999@gmail.com>
2020-12-06 00:46:11 +0700

    add skill prefab + casting mechanism + not add damge yet

---
+++ Assets/Scripts/Zombie/DestroySkill.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySkill : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "SelfDestroyLine")
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        Destroy(gameObject);
+
+
+    }
+
+
+}
